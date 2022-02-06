457f5290b274b1001a7fdad4869e53130b13595e
Samarth-Natrajan <97095980+Samarth-Natrajan@users.noreply.github.com>
2022-02-06 16:49:41 +0530

    Add files via upload

---
+++ CS_assignment_3output.py
@@ -0,0 +1,136 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+enter a string: samarth
+Occurence of s : 1
+Occurence of a : 2
+Occurence of m : 1
+Occurence of r : 1
+Occurence of t : 1
+Occurence of h : 1
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+enter a string: my name is name is is
+Occurence of my : 1
+Occurence of name : 2
+Occurence of is : 3
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter day in DD format: 12
+Enter month in MM format: 3
+Enter the year in YYYY format: 2020
+The next day:- 
+13/3/2020
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter day in DD format: 29
+Enter month in MM format: 2
+Enter the year in YYYY format: 2020
+The next day:- 
+01/3/2020
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter day in DD format: 31
+Enter month in MM format: 12
+Enter the year in YYYY format: 2025
+The next day:- 
+01/01/2026
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter day in DD format: 30
+Enter month in MM format: 5
+Enter the year in YYYY format: 2017
+Error: wrong input or date out of range
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter day in DD format: 30
+Enter month in MM format: 5
+Enter the year in YYYY format: 2014
+31 5 2014
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter day in DD format: 31
+Enter month in MM format: 4
+Enter the year in YYYY format: 2011
+Error: day out of range
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+enter number of elements you want in list: 4
+enter a number: 12
+enter a number: 23
+enter a number: 2
+enter a number: 9
+output
+ [(12, 144), (23, 529), (2, 4), (9, 81)]
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter a grade point in(4-10): 9
+Your Grade is A and Excellent performance
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter a grade point in(4-10): 2
+Error: input out of range!
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Traceback (most recent call last):
+  File "C:\Users\Jadu\Desktop\cs-assignment3.py", line 114, in <module>
+    for row in range(65,76-2*i):
+NameError: name 'i' is not defined
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Traceback (most recent call last):
+  File "C:\Users\Jadu\Desktop\cs-assignment3.py", line 116, in <module>
+    print(" "*i,l," "*i)
+NameError: name 'i' is not defined
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+ ABCDEFGHIJK 
+  ABCDEFGHI  
+   ABCDEFG   
+    ABCDE    
+     ABC     
+      A      
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+Enter SID of student: 21
+Enter their name: samarth
+Do you want to continue(Y/N): Y
+Enter SID of student: 34
+Enter their name: kohli
+Do you want to continue(Y/N): Y
+Enter SID of student: 22
+Enter their name: dhoni
+Do you want to continue(Y/N): N
+NAME 	 SID
+samarth 	 21
+kohli 	 34
+dhoni 	 22
+
+Sorted dictionary on basis of names:-
+{22: 'dhoni', 34: 'kohli', 21: 'samarth'} 
+
+Sorted dictonary on basis of SIDs:-
+{21: 'samarth', 22: 'dhoni', 34: 'kohli'}
+enter the SID for the student to be searched: 22
+Student found!!!
+dhoni
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+enter the limit till which the series is to be printed: 9
+Fibonacci series: 
+0,1,1,2,3,5,8,13,21,
+average of fibbonaci series is 6.0
+>>> 
+============== RESTART: C:\Users\Jadu\Desktop\cs-assignment3.py ==============
+{1, 3, 5, 6, 8} 
+
+{3, 6, 8, 9, 13} 
+
+{1, 2, 4, 5} 
+
+{6, 7, 8, 9, 10} 
+
+{10, 7} 
+
+>>> 
